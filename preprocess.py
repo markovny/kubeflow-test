035468bd18eef453128a431780325ce424658127
Qi Li <qi.li@marinethinking.com>
2023-08-23 11:26:29 -0300

    upload files

---
+++ preprocess.py
@@ -0,0 +1,20 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+import numpy as np
+
+def preprocess_data():
+    X, y = load_iris(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    np.save('x_train.npy', X_train)
+    np.save('x_test.npy', X_test)
+    np.save('y_train.npy', y_train)
+    np.save('y_test.npy', y_test)
+
+if __name__ == '__main__':
+    print('Preprocessing data...')
+    _preprocess_data()
